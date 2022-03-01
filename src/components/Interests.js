7f268271a9d33c3442c7f07788e435fc2de01ea1
Chrisson <chris.gao77@gmail.com>
2022-02-28 22:16:50 -0500

    Final

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ export default function Interests() {
     return (
         <div id = "interests">
             <h1>Interests</h1>
-            <p>I enjoy playing all sorts of games. This include sports, board games and video games! Furthermore
+            <p>I enjoy playing all sorts of games. This includes sports, board games and video games! Furthermore
                 I love to program and learn about new frameworks and languages.
             </p>
         </div>
